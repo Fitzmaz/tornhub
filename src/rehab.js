c2b58cd6e99d3851e48257491e36fc63b710a8a2
foo <zcrzcr@qq.com>
2020-12-25 23:48:42 +0800

    implement addiction points calculations

--- src/rehab.js
+++ src/rehab.js
@@ -12,6 +12,70 @@ function ajaxComplete(success) {
   });
 }
 
+const AddictionPoints = {
+  can: 1,
+  // ext,
+  ket: 7,
+  // lsd,
+  // opi,
+  // shr,
+  // spe,
+  // pcp,
+  xan: 35,
+  // vic,
+};
+const OverdosePoints = 100;
+const NaturalDecayPoints = 21;
+
+function naturalDecayTimes(fromDate, toDate) {
+  // Fri Dec 25 2020 12:00:00 GMT+0800 (中国标准时间)
+  const BaseTime = 1608868800000;
+  const Day = 1000 * 60 * 60 * 24;
+  let fromDays = Math.floor((new Date(fromDate).getTime() - BaseTime) / Day);
+  let toDays = Math.floor((new Date(toDate).getTime() - BaseTime) / Day);
+  console.log(toDays - fromDays);
+  return toDays - fromDays;
+}
+
+/*
+At 70 rehabs 1 rehab = 60 addiction points
+At 112 rehabs 1 rehab = 55 addiction points
+At 142 rehabs 1 rehab = 50 addiction points
+At 168 rehabs 1 rehab ~ 46.2 addiction points?
+At 190 rehabs 1 rehab ~ 42.75 addiction points?
+At 241 rehabs 1 rehab ~ 40 addiction points?
+At 283 rehabs 1 rehab = 35 addiction points
+At 287 rehabs 1 rehab = 33 addiction points
+At 356 rehabs 1 rehab ~ 31.6 addiction points
+At 360 - 454 rehabs 1 rehab ~ 30 addiction points
+At 529 rehabs 1 rehab = 24 addiction points
+At 543 rehabs 1 rehab = 24 addiction points
+At 655 rehabs 1 rehab = 24 addiction points
+At 701 rehabs 1 rehab = 20 addiction points (sticking out)
+At 731 rehabs 1 rehab = 24 addiction points (sticking out)
+At 735 rehabs 1 rehab = 24 addiction points (sticking out)
+At 854-1013 rehabs 1 rehab = 20 addiction points
+At 1077 rehabs 1 rehab ~ 18.83 addiction points?
+At 1965 rehabs 1 rehab = 15 addiction points?
+At 6586 rehabs 1 rehab ~ 2.4 or ~ 2.6 addiction points?
+At 10097 rehabs 1 rehab ~ 1.452 addiction points?
+At 11140 rehabs 1 rehab ~ 1.282 addiction points?
+At 24107 rehabs 1 rehab = 1.0 addiction points?
+*/
+function pointsPerRehab(totalRehabTimes) {
+  if (totalRehabTimes >= 70 && totalRehabTimes < 112) {
+    return 60;
+  } else {
+    // TODO:
+    return 0;
+  }
+}
+
+function calcPointsBeforeRehab(rehabPoints, lossPercentage) {
+  // 解毒百分比取整到小数点后两位，例如66.30%
+  return Math.round(((rehabPoints / (lossPercentage - 0.00005) + rehabPoints / (lossPercentage + 0.00005)) / 2));
+}
+
 const RehabRawDataKey = 'RehabRawDataKey';
 const RehabDataKey = 'RehabDataKey';
 let gSessionID;
@@ -102,6 +166,10 @@ function showReport(className) {
     { title: '总解毒格数', field: 'totalRehabTimes' },
     { title: 'xanan个数', field: 'xantaken' },
     { title: 'OD次数', field: 'overdosed' },
+    { title: '当前剩余AP', field: 'remainingPoints' },
+    { title: '嗑药增加AP', field: 'drugPoints' },
+    { title: '自然消退AP', field: 'decayPoints' },
+    { title: 'AP变化量', field: 'deltaPoints' },
   ];
   let data = storage.get(RehabDataKey) || {};
   // fetchDrugsInfo if needed
@@ -110,9 +178,36 @@ function showReport(className) {
     fetchDrugsInfo(latestSessionID);
   }
   // format rehabDate
-  let rows = Object.values(data).map(record => {
+  let rows = Object.values(data).map((record, index, dataArray) => {
+    let drugPoints;
+    let decayPoints;
+    let remainingPoints;
+    let deltaPoints;
+    if (index > 0) {
+      let lastRecord = dataArray[index - 1];
+      drugPoints = Object.keys(AddictionPoints).reduce((acc, val, idx) => {
+        let ap = Math.ceil(0.5 * AddictionPoints[val]); // faction perk - 50% ap
+        let key = `${val}taken`;
+        let drugsTaken = record[key] - lastRecord[key];
+        let addictions = drugsTaken * ap;
+        return acc + addictions;
+      }, 0);
+      decayPoints = naturalDecayTimes(lastRecord.rehabDate, record.rehabDate) * NaturalDecayPoints;
+
+      // 上次解毒后到这次解毒前points的变化量
+      remainingPoints = calPoints(record).pointsRemaining;
+      deltaPoints = calPoints(record).pointsBeforeRehab - calPoints(lastRecord).pointsRemaining;
+      function calPoints(record) {
+        let rehabPoints = pointsPerRehab(record.totalRehabTimes) * record.rehabTimes;
+        let lossPercentage = Number(record.addictionLoss.split("%")[0]) / 100;
+        let pointsBeforeRehab = calcPointsBeforeRehab(rehabPoints, lossPercentage);
+        let pointsRemaining = pointsBeforeRehab - rehabPoints;
+        return { pointsBeforeRehab, pointsRemaining };
+      }
+    }
+
     let rehabDate = record.rehabDate ? new Date(record.rehabDate).toLocaleString([], { hour12: false }) : '-';
-    return Object.assign(record, { rehabDate });
+    return Object.assign(record, { rehabDate, drugPoints, decayPoints, remainingPoints, deltaPoints });
   })
   let el = createReportTable(cols, rows);
   el.className = className;
